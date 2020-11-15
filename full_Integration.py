9a38ae12fea7b5ab19ca6ff01c985590fe1d84eb
Dashy999 <60180014+Dashy999@users.noreply.github.com>
2020-11-16 00:46:12 +1100

    finalIntegration

--- full_Integration.py
+++ full_Integration.py
@@ -12,6 +12,12 @@ import matplotlib.pyplot as plt
 import idx2numpy
 import math
 
+from sklearn import svm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+from skimage.feature import hog
+import pickle
 
 ################################################################################
 # Globals
@@ -21,9 +27,11 @@ global lineMatrix
 global scoreMatrix
 global nRows
 global nCols
-global playerColour
 global playerColourRGB
 global playerTurn
+global playerColour
+global colourLabel 
+global playerScore
 
 
 # kernel = 15
@@ -72,7 +80,7 @@ def captureImg(cap, show_img=0):
   ret, frame = cap.read()
   frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
 
-  return frame
+  return rval, frame
 
 def checkBlank(im):
 
@@ -92,10 +100,10 @@ def checkBlank(im):
     mean_list.append(np.mean(sub_im))
 
   if abs(mean_list[0]-mean_list[1]) > threshold or abs(mean_list[0]-mean_list[2]) > threshold:
-    print('Not blank!')
+    #print('Not blank!')
     return 0
   else:
-    print('Blank')
+    #print('Blank')
     return 1
 
 def detectFinger(im):
@@ -176,11 +184,9 @@ def getNumPlayers(vc,text_display):
   WIDTH = 1
   HEIGHT = 2
 
-  rval, frame = vc.read()
-  frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+  rval, frame = captureImg(vc)
   while rval:
-    rval, frame = vc.read()
-    frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+    rval, frame = captureImg(vc)
 
     # DETECTING FINGER
     draw_sub_im = frame[draw_box_y1:draw_box_y2,draw_box_x1:draw_box_x2]
@@ -218,7 +224,7 @@ def getNumPlayers(vc,text_display):
     if curr_region == prev_region and curr_region != -1:
       region_count += 1
       if region_count > region_thresh:
-        if getConfirmation(vc,'Lock in number of players: '+str(curr_region)) == True:
+        if getConfirmation(vc,'Lock in number of players: '+str(curr_region), 0) == True:
           return curr_region
         else:
           # Reset values and restart
@@ -228,13 +234,12 @@ def getNumPlayers(vc,text_display):
           finger_loc = (-1,-1)
 
           # Load most updated image and then output.
-          rval, frame = vc.read()
-          frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+          rval, frame = captureImg(vc)
     else:
       region_count = 0
 
     prev_region = curr_region
-    print(region_count)
+    #print(region_count)
 
     cv2.imshow("preview", frame)
     key = cv2.waitKey(20)
@@ -247,11 +252,9 @@ def getCharacter(vc,text_display):
   prev_status = False
 
   drawn_char = []
-  rval, frame = vc.read()
-  frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+  rval, frame = captureImg(vc)
   while rval:
-    rval, frame = vc.read()
-    frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+    rval, frame = captureImg(vc)
 
     # DETECTING FINGER
     draw_sub_im = frame[draw_box_y1:draw_box_y2,draw_box_x1:draw_box_x2]
@@ -270,7 +273,7 @@ def getCharacter(vc,text_display):
     else: string_display = text_display
 
 
-    print(draw_char_status)
+    #print(draw_char_status)
 
 
     if draw_char_status == True:
@@ -281,8 +284,11 @@ def getCharacter(vc,text_display):
       drawn_char = removeListValues(drawn_char,(-1,-1))
       char_dict[num_chars] = drawn_char
 
-      if getConfirmation(vc,'Lock in the drawn character?') == True:
-        return char_dict
+      letter = convertToLetter(char_dict)
+      letterIndex = playerColour.index(letter)
+
+      if getConfirmation(vc,'Lock in the colour: ' + colourLabel[letterIndex], letterIndex) == True:
+        return letter
       else:
         # Reset values and restart
         # draw_char_status = False
@@ -291,8 +297,7 @@ def getCharacter(vc,text_display):
         char_dict = dict()
 
         # Load most updated image and then output.
-        rval, frame = vc.read()
-        frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+        rval, frame = captureImg(vc)
 
     prev_status = draw_char_status
 
@@ -306,7 +311,7 @@ def getCharacter(vc,text_display):
     if key == 27: # exit on ESC
       break
 
-def getConfirmation(vc,text_display):
+def getConfirmation(vc,text_display, letterIndex):
   region_thresh = 40
   prev_region = -1
   curr_region = -1
@@ -326,11 +331,9 @@ def getConfirmation(vc,text_display):
   WIDTH = 1
   HEIGHT = 2
 
-  rval, frame = vc.read()
-  frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+  rval, frame = captureImg(vc)
   while rval:
-    rval, frame = vc.read()
-    frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+    rval, frame = captureImg(vc)
 
     # DETECTING FINGER
     draw_sub_im = frame[draw_box_y1:draw_box_y2,draw_box_x1:draw_box_x2]
@@ -346,7 +349,7 @@ def getConfirmation(vc,text_display):
     cv2.rectangle(frame,(flag_box_x1,flag_box_y1),(flag_box_x2,flag_box_y2),color=(0,0,0,0))
 
     # Intructions for user
-    cv2.putText(frame,text_display,(width//5,height*4//5), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),3,cv2.LINE_AA)
+    cv2.putText(frame,text_display,(width//5,height*4//5), cv2.FONT_HERSHEY_SIMPLEX, 2, playerColourRGB[letterIndex],3,cv2.LINE_AA)
 
     # Draw options in box
     for r in [1,0]:
@@ -377,7 +380,7 @@ def getConfirmation(vc,text_display):
       region_count = 0
 
     prev_region = curr_region
-    print(region_count)
+    #print(region_count)
 
     cv2.imshow("preview", frame)
     key = cv2.waitKey(20)
@@ -386,13 +389,13 @@ def getConfirmation(vc,text_display):
 
 #-------------------------------------------------------------------------------
 # drawGrid
-def drawGrid(frame, dotsRows, dotsCols):
+#def drawGrid(frame, dotsRows, dotsCols):
 
-  for a in range(dotsRows) :
+#  for a in range(dotsRows) :
 
-    for b in range(dotsCols) :
-      #image = cv2.circle(image, center_coordinates, radius, color, thickness)
-      frame = cv2.circle(frame, (round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows))), 20, (0, 0, 0), 5)
+#    for b in range(dotsCols) :
+#      #image = cv2.circle(image, center_coordinates, radius, color, thickness)
+#      frame = cv2.circle(frame, (round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows))), 20, (0, 0, 0), 5)
 
 #-------------------------------------------------------------------------------
 # drawLine
@@ -404,26 +407,238 @@ def drawLines(frame, dotsRows, dotsCols):
       for c in range(2) :
         
         if lineMatrix[a][b][c] != 0 :
+
+          xAssign1 = b
+          yAssign1 = a
+
+          dotPoint1 = (xAssign1,yAssign1)
+          coord1 = convertDot2Coord(dotPoint1)
+
           colourIndex = int(lineMatrix[a][b][c] - 1)
           if c == 0:
-            frame = cv2.line(frame, ( round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows)) ), ( round(100+(b+1)*(800/dotsCols)), round(100+a*(800/dotsRows)) ), playerColourRGB[colourIndex], 5)
+            xAssign2 = b+1
+            yAssign2 = a
+
+            dotPoint2 = (xAssign2,yAssign2)
+            coord2 = convertDot2Coord(dotPoint2)
+            #frame = cv2.line(frame, ( round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows)) ), ( round(100+(b+1)*(800/dotsCols)), round(100+a*(800/dotsRows)) ), playerColourRGB[playerColour.index(assigned_players[colourIndex])], 5)
+            frame = cv2.line(frame, coord1, coord2, playerColourRGB[playerColour.index(assigned_players[colourIndex])], 5)
 
           elif c == 1:
-            frame = cv2.line(frame, ( round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows)) ), ( round(100+b*(800/dotsCols)), round(100+(a+1)*(800/dotsRows)) ), playerColourRGB[colourIndex], 5)
+            xAssign2 = b
+            yAssign2 = a+1
+
+            dotPoint2 = (xAssign2,yAssign2)
+            coord2 = convertDot2Coord(dotPoint2)
+            #frame = cv2.line(frame, ( round(100+b*(800/dotsCols)), round(100+a*(800/dotsRows)) ), ( round(100+b*(800/dotsCols)), round(100+(a+1)*(800/dotsRows)) ), playerColourRGB[playerColour.index(assigned_players[colourIndex])], 5)
+            frame = cv2.line(frame, coord1, coord2, playerColourRGB[playerColour.index(assigned_players[colourIndex])], 5)
 
         
   # Drawing in the text for a complete square
   for a in range(dotsRows-1) :
     for b in range(dotsCols-1) :
       if scoreMatrix[a][b] != 0 :
-            colourIndex = int(scoreMatrix[a][b] - 1)
-            frame = cv2.putText(frame, str(int(scoreMatrix[a][b])) , ( round(150+b*(800/dotsCols)), round(150+a*(800/dotsRows)) ), cv2.FONT_HERSHEY_SIMPLEX , 1, playerColourRGB[colourIndex] , 2, cv2.LINE_AA) 
+            xAssign = b
+            yAssign = a
+            dotPointScore = (xAssign,yAssign)
+            coordScore = convertDot2Coord(dotPointScore)
 
+            colourIndex = int(scoreMatrix[a][b] - 1)
+            frame = cv2.putText(frame, str(int(scoreMatrix[a][b])) , ( round(25+coordScore[0]), round(25+coordScore[1]) ), cv2.FONT_HERSHEY_SIMPLEX , 1, playerColourRGB[playerColour.index(assigned_players[colourIndex])] , 2, cv2.LINE_AA) 
+
+#--------------------------------------------------------------------------------------
+def convertToLetter(char_dict) :
+
+    featureList = []
+
+    filename = './ROGM_model.sav'
+		
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    for x in char_dict:
+        currentSet = char_dict[x]
+        image = np.zeros((432,432))
+
+        for n in currentSet:
+
+            for i in range(-10,10) :
+                for j in range(-10,10) :
+                    if (n[1] + i) >= 0 and (n[1] + i) <= 431 and (n[0] + j) >= 0 and (n[0] + j) <= 431 :
+                                            
+                        image[n[1]+i,n[0]+j] = 1
+
+        resizedIm = cv2.resize(image,(28,28))
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+        morphedIm = cv2.dilate(resizedIm,kernel,iterations = 1)
+        kernel = np.ones((2,2),np.float32)/4
+        dst = cv2.filter2D(morphedIm,-1,kernel)
+
+        hog_Feature = hog(dst, orientations=8, pixels_per_cell=(4, 4), cells_per_block=(1, 1), visualize=False, multichannel=False)
+
+        featureList.append(hog_Feature)
+        y_pred = loaded_model.predict(featureList)
+                        
+
+    print(y_pred)
+    return y_pred[0]
+
+# checkValidInput
+def checkValidInput(x1,y1,x2,y2):
+       
+    validInput = 0
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    # Check to see if valid line (e.g. coordinates are in grid and adjacent)
+    if x1 <= nCols and x2 <= nCols and y1 <= nRows and y2 <= nRows and distance == 1:
+
+        # Check to see if line is free or occupied by another player
+        if x1 == x2 and lineMatrix[min(y1,y2)-1,x1-1,1] < 1 :
+            lineMatrix[min(y1,y2)-1,x1-1,1] = playerTurn
+            validInput = 1
+            #plt.plot([x1, x2], [y1, y2], playerColour[playerTurn-1], linewidth = 10)
+        elif y1 == y2 and lineMatrix[y1-1,min(x1,x2)-1,0] < 1 :
+            lineMatrix[y1-1,min(x1,x2)-1,0] = playerTurn
+            validInput = 1
+            #plt.plot([x1, x2], [y1, y2], playerColour[playerTurn-1], linewidth = 10)
+        else :
+            print('Line already occupied')
+
+    else :
+        print('Invalid input')
+
+    return validInput
+
+#-------------------------------------------------------------------------------
+# updateScore
+def updateScore():
+
+    playerScored = 0
+
+    # Check to see if complete square and increase score if it is
+    for i in range(nRows) : 
+        for j in range(nCols) :    
+            if lineMatrix[i-1,j-1,0] > 0 and lineMatrix[i-1,j-1,1] > 0  and lineMatrix[i,j-1,0] > 0 and lineMatrix[i-1,j,1] > 0 and scoreMatrix[i-1,j-1] == 0 :
+                   
+                scoreMatrix[i-1,j-1] = playerTurn
+                playerScored = 1 
+
+                #plt.plot(j+0.5, i+0.5, 'x', linewidth = 10, color = playerColour[playerTurn-1])  
+
+                playerScore[playerTurn-1] += 1
+
+    return playerScored
+
+# -----------------------------------------------------------------------------
+def get2Points(vc):
+    dot1 = get1Point(vc,'Select Point 1')
+    dot2 = get1Point(vc,'Select Point 2',point1=convertDot2Coord(dot1))
+    return [dot1,dot2]
+
+def convertDot2Coord(dot):
+    offset = 100
+    max_w = abs(draw_box_x1-draw_box_x2)
+    dotsCols = 4
+    space = (max_w-offset)//dotsCols
+    dot_r = 20
+
+    x = dot[0]
+    y = dot[1]
+
+    coord = (draw_box_x1+x*space+offset+dot_r,draw_box_y1+y*space+offset+dot_r)
+    return coord
+
+def get1Point(vc,text_display,point1=None):
+    region_thresh = 40
+    prev_region = -1
+    curr_region = -1
+    region_count = 0
+    finger_loc = (-1,-1)
+
+    region_bounds = getDotBounds((draw_box_x1,draw_box_y1),abs(draw_box_x1-draw_box_x2)-100)
+
+    # Draw options in box
+    COORD = 0
+    WIDTH = 1
+    HEIGHT = 2
+
+    rval, frame = captureImg(vc)
+    while rval:
+        rval, frame = captureImg(vc)
+
+        # DETECTING FINGER
+        draw_sub_im = frame[draw_box_y1:draw_box_y2,draw_box_x1:draw_box_x2]
+        rel_finger_loc = detectFinger(draw_sub_im)    # Relative finger point
+
+        # DRAWING FINGER POINT
+        if rel_finger_loc != (-1,-1):
+            finger_loc = (rel_finger_loc[0]+draw_box_x1,rel_finger_loc[1])
+            frame = cv2.circle(frame,finger_loc,radius,(255,0,0),-1)
+        
+        # Drawing boxes and text
+        cv2.rectangle(frame,(draw_box_x1,draw_box_y1),(draw_box_x2,draw_box_y2),color=playerColourRGB[playerColour.index(assigned_players[playerTurn-1])])
+        cv2.rectangle(frame,(flag_box_x1,flag_box_y1),(flag_box_x2,flag_box_y2),color=playerColourRGB[playerColour.index(assigned_players[playerTurn-1])])
+        cv2.putText(frame,text_display,(width//5,height*4//5), cv2.FONT_HERSHEY_SIMPLEX, 2, playerColourRGB[playerColour.index(assigned_players[playerTurn-1])],3,cv2.LINE_AA)
+        cv2.putText(frame, 'Player ' + str(playerTurn) + ' turn:' , (50, 50) , cv2.FONT_HERSHEY_SIMPLEX , 2, playerColourRGB[playerColour.index(assigned_players[playerTurn-1])] , 2, cv2.LINE_AA) 
+        
+        # DRAW GRID AND IDENTIFY WHERE THE FINGER IS
+        frame = drawGrid(frame,region_bounds,20)
+        drawLines(frame, nRows, nCols)
+        for r in range(16):
+            if checkIfInBound(finger_loc,region_bounds[r]):
+                curr_region = r
+                break
+            else: curr_region = -1
+
+        # Track finger position over time
+        if curr_region == prev_region and curr_region != -1:
+            region_count += 1
+            if region_count > region_thresh:
+                return (curr_region%4,curr_region//4)
+        else:
+            region_count = 0
+
+        prev_region = curr_region
+
+
+        # Draw line from current finger position to the previous selected point (if exists)
+        if point1 is not None: cv2.line(frame,point1,finger_loc,(0,0,0),3)
+
+        cv2.imshow("preview", frame)
+        key = cv2.waitKey(20)
+        if key == 27:
+            break
+
+def drawGrid(frame,region_bounds,dot_r):
+    for i in range(len(region_bounds)):
+        coord = (region_bounds[i][0],region_bounds[i][0])
+        coord = (coord[0][0]+dot_r,coord[0][1]+dot_r)
+        frame = cv2.circle(frame, coord, dot_r, (0, 0, 0), 3)
+    return frame
+
+def getDotBounds(ref_coord, max_w):
+    ref_x = ref_coord[0]
+    ref_y = ref_coord[1]
+    offset = 100
+    dot_r = 20
+    region_bounds = dict()
+
+    i = 0
+    for a in range(nRows):
+        for b in range(nCols):
+            dot_coord_x = ref_x+offset+b*(max_w//nCols)
+            dot_coord_y = ref_y+offset+a*(max_w//nRows)
+            region_bounds[i] = [(dot_coord_x,dot_coord_y),dot_r*2,dot_r*2]
+            i += 1
+    return region_bounds   
 ################################################################################
 # Main
 ################################################################################
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
+
+playerColour = [18, 15, 7, 13]
+colourLabel = ['Red', 'Orange', 'Green', 'Magenta']
+playerColourRGB = [(0, 0, 255), (0,165,255), (0,128,0), (255,0,255)]
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -446,20 +661,33 @@ flag_box_y1 = 0
 flag_box_y2 = height//5
 
 num_players = getNumPlayers(vc,'Select number of players')
-char_dict = getCharacter(vc,'Input colour. Awaiting Signal.')
 
-nRows = 5
-nCols = 5
+assigned_players = np.zeros(num_players)
+num = 0
+
+
+while num < num_players :
+    letter = getCharacter(vc,'Input colour for Player ' + str(num+1) )
+    if letter in assigned_players : 
+        print('Choose another letter')
+    else:
+        assigned_players[num] = letter
+        num += 1
+
+
+nRows = 4
+nCols = 4
 
 lineMatrix = np.zeros((nRows,nCols,2))
 scoreMatrix = np.zeros((nRows-1,nCols-1))
+playerScore = np.zeros(num_players)
 
 playerTurn = 1
 playerScored = 0
 
-playerColour = ['k','r','g']
-playerColourRGB = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-currentInput = ['x1','y1','x2','y2']
+#playerColour = ['r', 'o', 'g', 'm']
+#playerColour = [18, 15, 7, 13]
+
 inputTracker = 0
 validInput = 0
 
@@ -468,14 +696,47 @@ print("Player " + str(playerTurn) + " turn:")
 while True:
 
 
-  frame = captureImg(vc)
-  # Drawing all the lines on the grid
-  drawGrid(frame, nRows, nCols)
-  drawLines(frame, nRows, nCols)
-  # Text showing player and displaying current image
-  frame = cv2.putText(frame, str(playerTurn) + ': ' + currentInput[inputTracker], (25, 25) , cv2.FONT_HERSHEY_SIMPLEX , 1, playerColourRGB[playerTurn-1] , 2, cv2.LINE_AA) 
-  cv2.imshow('Captured Image', frame)
-  c = cv2.waitKey(1)
+    #rval, frame = captureImg(vc)
+
+    # Drawing all the lines on the grid
+    #drawGrid(frame, nRows, nCols)
+    #drawLines(frame, nRows, nCols)
+
+    # Text showing player and displaying current image
+    #frame = cv2.putText(frame, str(playerTurn) + ':' , (25, 25) , cv2.FONT_HERSHEY_SIMPLEX , 1, playerColourRGB[playerColour.index(assigned_players[playerTurn-1])] , 2, cv2.LINE_AA) 
+    #cv2.imshow('preview', frame)
+    #c = cv2.waitKey(1)
+
+    [dot1,dot2] = get2Points(vc)
+
+    x1 = dot1[0] + 1
+    y1 = dot1[1] + 1
+    x2 = dot2[0] + 1
+    y2 = dot2[1] + 1
+
+    #drawLines(frame, nRows, nCols)
+
+    validInput = checkValidInput(x1,y1,x2,y2)
+
+    if validInput == 1 :
+
+        playerScored = updateScore()
+
+        # Current Player 
+        if playerScored == 0:
+            playerTurn = playerTurn + 1
+        else :
+            playerScored = 0
+              
+        if playerTurn > num_players :
+            playerTurn = 1
+            
+          
+        print("Player " + str(playerTurn) + " turn:")
+        validInput = 0
+
+
+      
 
 vc.release()
-cv2.destroyWindow("preview") 
+cv2.destroyWindow('preview') 
---
+++ viewCamera.py
@@ -0,0 +1,87 @@
+################################################################################
+
+# Alpacas & Fences - viewCamera
+# Authors: 470203101, 470386390, 470345850
+
+# In order to run this file alone:
+# $ python viewCamera.py
+
+# This script aids in the collection of web camera images.
+
+# Code modified from https://subscription.packtpub.com/book/application_development/9781785283932/3/ch03lvl1sec28/accessing-the-webcam
+
+################################################################################
+# Imports
+################################################################################
+import cv2
+
+################################################################################
+# Constants
+################################################################################
+
+################################################################################
+# Functions
+################################################################################
+# camStart
+# Returns the camera image capturing object.
+# Input:
+#   - N/A
+# Output:
+#   - cap -> camera image capturing object
+def camStart():
+  cap = cv2.VideoCapture(0)
+  return cap
+
+#-------------------------------------------------------------------------------
+# captureImg
+# Captures and image from the web camera using the given camera image capturing object
+# Input:
+#   - cap -> camera image capturing object
+# Output:
+#   - N/A
+# Note:
+#   - Captured image is shown on the screen.
+def captureImg(cap, show_img=0):
+  # Check if the webcam is opened correctly
+  if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+  ret, frame = cap.read()
+  frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+  #frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+
+  if show_img:
+    cv2.imshow('Captured Image', frame)
+
+  return frame
+
+#-------------------------------------------------------------------------------
+# camFinish
+# Releases the camera image capturing object and closes all open image showing windows.
+# Input:
+#   - cap -> camera image capturing object
+# Output:
+#   - N/A
+def camFinish(cap):
+  cap.release()
+  cv2.destroyAllWindows()
+
+################################################################################
+# Main
+################################################################################
+if __name__ == "__main__":
+  print("[viewCamera - INFO]: Starting viewCamera.py")
+  print("[viewCamera - HELP]: To stop the webcam stream, please hit the esc key.")
+
+  cap = camStart()
+
+  while True:
+    captureImg(cap,1)
+    c = cv2.waitKey(1)
+    if c == 27: # esc key
+        break
+
+  camFinish(cap)
+  print("[viewCamera - INFO]: Finished viewCamera.py")
+
+
